53709bf3241b7f0167ddbaaeae54ab6a8dd18d81
ztao <t@taozj.org>
2020-02-12 16:46:41 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+example to use grpc in sync (one-thread-pre-request) and async (threadpool) mode.
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "[INFO] begin grpc_async demo test." << std::endl;
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
