3ccb3f3c392f56310155b566b72daf988bb8d4be
ztao <t@taozj.org>
2020-02-12 18:03:35 +0800

    implement sync server

--- main.cpp
+++ main.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <cstdlib>
 
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/server_builder.h>
+
+const std::string address = "0.0.0.0:19988";
+
+#include "service_impl.h"
+
+static int RunServer() {
+
+    ServiceImpl service {};
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    std::cout << "[INFO] server started at " << address << std::endl;
+
+    // loop forever
+    server->Wait();
+
+    return 0;
+}
+
+
+
 int main(int argc, char* argv[]) {
 
     std::cout << "[INFO] begin grpc_async demo test." << std::endl;
 
 
+    // Sync
+    RunServer();
 
-
+    // Async
 
     return EXIT_SUCCESS;
 }
---
+++ service_impl.h
@@ -0,0 +1,30 @@
+#ifndef __SERVICE_IMPL_
+#define __SERVICE_IMPL_
+
+#include "arithmetic_service.pb.h"
+#include "arithmetic_service.grpc.pb.h"
+
+
+class ServiceImpl final : public Arithmetic::Interface::Service {
+
+public:
+    grpc::Status GetSum(grpc::ServerContext* context,
+            const Arithmetic::ReqOperand* req,
+            Arithmetic::RspOperand* rsp) override {
+
+        rsp->set_num(req->num_1() + req->num_2());
+        return grpc::Status::OK;
+    }
+
+    grpc::Status GetSub(grpc::ServerContext* context,
+                        const Arithmetic::ReqOperand* req,
+                        Arithmetic::RspOperand* rsp) override {
+
+        rsp->set_num(req->num_1() - req->num_2());
+        return grpc::Status::OK;
+    }
+
+};
+
+
+#endif // __SERVICE_IMPL_
